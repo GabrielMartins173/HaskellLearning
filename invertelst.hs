125917c0b26863097bac17b3e4a2495244f55b34
Gabriel Martins dos Santos <gabrielmartins173@gmail.com>
2021-02-07 22:34:14 +0000

    deleting extra files and using only main

--- invertelst.hs
+++
@@ -1,6 +0,0 @@
---This function receives as input one list of any type and 
--- returns the list with the elements on the inverted order. 
-
-invertelst:: [any] -> [any]
-invertelst [] = []
-invertelst (head:tail) = (invertelst tail)++[head]
--- main.hs
+++ main.hs
@@ -1,3 +1,10 @@
+--This function receives as input one list of any type and 
+-- returns the list with the elements on the inverted order. 
+
+invertelst:: [any] -> [any]
+invertelst [] = []
+invertelst (head:tail) = (invertelst tail)++[head]
+
 main :: IO()
 main = do
         putStrLn "Invert a List of Integer numbers: "
