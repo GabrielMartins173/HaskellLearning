80d696f0ab4af2ca8788f9333b863202ac4dd9ac
Gabriel Martins dos Santos <gabrielmartins173@gmail.com>
2021-02-14 22:04:57 +0000

    new implementations and organization

--- main.hs
+++ main.hs
@@ -1,20 +1,64 @@
--- This function receives as a parameter a list of numbers and 
--- returns one float number that is the sum of all numbers on this 
--- input list. 
 
-somatorio :: [float] -> float
-somatorio :: (float a) => [a] -> a  
-somatorio xs = foldl (\acc x -> acc + x) 0 xs 
 
 -- main function to test the implemented function. 
 
 main :: IO()
 main = do
 
+        let list = [0.1, 0.2, 0.3, 0.4]
+
         -- test for the somatorio function
-        putStrLn "Sum the numbers on the array of integers: "
-        print(somatorio [1,2,3,4,5,6,7,8,9,10])
+        putStrLn "Norm for the numbers 0.1, 0.2, 0.3, 0.4 should be 0.5477226"
+        print(norma list)
         putStrLn " "
-        putStrLn "Sum the numbers on the array of floats: "
-        print(somatorio [1.5,2.3,3.4,4.6,5.4,6.789,7.14,8.364,9.214,10.164654])
+        putStrLn "Norm of an empty list should be Zero "
+        print(norma [])
         putStrLn " "
+
+
+
+
+
+
+
+
+-- This function receives as a parameter one list of float numbers between 
+-- zero and 1, and returns the norm of this numbers. 
+
+norma :: [Float] -> Float
+norma ns = sqrt (sum (map (^2) ns))
+
+
+-- This function receives as a parameter a list of numbers and 
+-- returns one float number that is the sum of all numbers on this 
+-- input list. 
+
+somatorio :: [Float] -> Float
+somatorio [] = 0
+somatorio (h:t) = h + (foldr (+) 0 t)
+
+
+-- This function receives as input one list of any type and 
+-- returns the list with the elements on the inverted order. 
+
+invertelst :: [any] -> [any]
+invertelst [] = [] -- If we receive an empty list we should return an empty list.
+invertelst (head:tail) = (invertelst tail)++[head]
+     
+
+-- This function is used as auxiliar function for the 
+-- powlist function, this is only the implementing 
+-- of the potentiation operation having any positive float number  
+-- as base and one integer number as expoent.  
+
+pow :: Float -> Int -> Float
+pow base expoent | (expoent == 0) = 1 -- If the expoent is equal to 0 we should return 1. 
+    | otherwise = base ^ expoent
+
+-- This function receives as input one list of numbers and one integer number
+-- and returns the list of numbers with all the elements potentialized using 
+-- the integer number as an expoent. 
+
+powlist :: [Float] -> Int -> [Float]
+powlist [] expoent = [] -- If we receive an empty list we should return an empty list. 
+powlist (head:tail) expoent = [head^expoent] ++ powlist tail expoent
